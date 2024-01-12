f305ef8e3d8a275381764fbed97b1c938ae6b1ad
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2024-01-12 14:48:33 +0000

    292601 - Added more test cases for results screen, organized scss files

--- tests/routes/web/results.spec.ts
+++ tests/routes/web/results.spec.ts
@@ -9,7 +9,7 @@ import { Server } from '@hapi/hapi';
 import { initializeServer } from '../../../src/infrastructure/server';
 import { webRoutePaths } from '../../../src/utils/constants';
 
-describe('Results Routes', () => {
+describe('Results Screen', () => {
   let server: Server;
 
   beforeAll((done) => {
@@ -23,39 +23,215 @@ describe('Results Routes', () => {
     server.stop().then(() => done());
   });
 
-  it('should match the /results snapshot', async () => {
-    const options = {
-      method: 'GET',
-      url: webRoutePaths.results,
-    };
+  describe('Results > Sanpshot verification', () => {
+    it('should match the results screen snapshot', async () => {
+      const options = {
+        method: 'GET',
+        url: webRoutePaths.results,
+      };
 
-    const response = await server.inject(options);
-    expect(response.payload).toMatchSnapshot();
+      const response = await server.inject(options);
+      expect(response.payload).toMatchSnapshot();
+    });
   });
 
-  it('should GET /results route works with status code 200', async () => {
-    const options = {
-      method: 'GET',
-      url: webRoutePaths.results,
-    };
+  describe('Results > Check /results route status code', () => {
+    it('should / route works with status code 200', async () => {
+      const options = {
+        method: 'GET',
+        url: webRoutePaths.results,
+      };
 
-    const response = await server.inject(options);
-    expect(response.statusCode).toEqual(200);
+      const response = await server.inject(options);
+      expect(response.statusCode).toEqual(200);
+    });
   });
-  it('should check for the text Search', async () => {
-    const options = {
-      method: 'GET',
-      url: webRoutePaths.results,
-    };
 
-    const response = await server.inject(options);
-    const rawHTML = response.payload;
-    const parser = new DOMParser();
-    const document = parser.parseFromString(rawHTML, 'text/html');
-    expect(
-      document
-        ?.querySelector('.quick_search-container__heading-m')
-        ?.textContent?.trim()
-    ).toBe('Search');
+  describe('Results > Breadcrumb block', () => {
+    describe('Breadcrumb classes', () => {
+      it('renders the container class', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        expect(document.querySelector('.govuk-breadcrumbs')).toBeTruthy();
+      });
+
+      it('renders the breadcrumb list class', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        expect(document.querySelector('.govuk-breadcrumbs__list')).toBeTruthy();
+      });
+    });
+
+    describe('Breadcrumb list items', () => {
+      it('should render 2 list items', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        const breadcrumbList = document?.querySelector(
+          '.govuk-breadcrumbs__list'
+        );
+        expect(breadcrumbList?.childElementCount).toEqual(2);
+      });
+    });
+
+    describe('Breadcrumb list item options', () => {
+      it('should render home list item as a first child', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        const item = document.querySelector('.govuk-breadcrumbs__list')
+          ?.firstElementChild;
+        const anchor = item?.firstElementChild;
+        expect(anchor?.tagName.toLowerCase()).toBe('a');
+        expect(anchor?.getAttribute('class')).toEqual(
+          'govuk-breadcrumbs__link'
+        );
+        expect(anchor?.getAttribute('href')).toEqual(webRoutePaths.home);
+        expect(anchor?.textContent?.trim()).toEqual('Home');
+      });
+    });
+
+    it('should render search list item as a second child', async () => {
+      const options = {
+        method: 'GET',
+        url: webRoutePaths.results,
+      };
+
+      const response = await server.inject(options);
+      const rawHTML = response.payload;
+      const parser = new DOMParser();
+      const document = parser.parseFromString(rawHTML, 'text/html');
+      const item = document.querySelector('.govuk-breadcrumbs__list')
+        ?.lastElementChild;
+      const anchor = item?.firstElementChild;
+      expect(anchor?.tagName.toLowerCase()).toBe('a');
+      expect(anchor?.getAttribute('class')).toEqual('govuk-breadcrumbs__link');
+      expect(anchor?.getAttribute('href')).toEqual('#');
+      expect(anchor?.textContent?.trim()).toEqual('Search results');
+    });
+  });
+
+  describe('Results > Search block', () => {
+    describe('Search block classes', () => {
+      it('renders custom quick search container class', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        expect(document.querySelector('.quick_search-container')).toBeTruthy();
+      });
+
+      it('renders custom search block class', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.home,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        expect(document.querySelector('.search-block')).toBeTruthy();
+      });
+    });
+
+    describe('Search block custom options', () => {
+      it('renders attributes correctly', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        const searchBlock = document.querySelector('.search-block');
+        expect(searchBlock?.getAttribute('data-module')).toEqual('search');
+      });
+    });
+
+    describe('Search container content elements', () => {
+      it('should render 2 child elements', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        const bannerContainer = document?.querySelector(
+          '.quick_search-container'
+        );
+        expect(bannerContainer?.childElementCount).toEqual(2);
+      });
+
+      it('should not render caption on results screen', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        expect(
+          document?.querySelector('.quick_search-container__caption-m')
+        ).toBeNull();
+      });
+    });
+
+    describe('Search container block heading', () => {
+      it('should render the search container heading', async () => {
+        const options = {
+          method: 'GET',
+          url: webRoutePaths.results,
+        };
+
+        const response = await server.inject(options);
+        const rawHTML = response.payload;
+        const parser = new DOMParser();
+        const document = parser.parseFromString(rawHTML, 'text/html');
+        expect(
+          document
+            ?.querySelector('.quick_search-container__heading-m')
+            ?.textContent?.trim()
+        ).toBe('Search');
+      });
+    });
   });
 });
