dbab239cd37d628be8b4a365d7ae0b4d13614045
karthigayayiniram <149910290+karthigayayiniram@users.noreply.github.com>
2024-07-24 15:06:18 +0100

    Fix/407211 fix classifiers api endpoint (#110)
    * feat: GTM tracking
    
    * fix: Keyvalue name
    
    * API endpoint fixes
    
    * API fixes
    
    * API fixes

--- src/services/handlers/classifierApi.ts
+++ src/services/handlers/classifierApi.ts
@@ -31,7 +31,8 @@ const invokeClassifierApi = async (level: string, parents: string = ''): Promise
     const classifierApiAuthKey = await getSecret(
       environmentConfig.classifierApiKey ?? 'nceaClassifierMicroServiceApiKey',
     );
-    let url = `${environmentConfig.classifierApiUrl}api/classifiers?level=${level}`;
+
+    let url = `${environmentConfig.classifierApiUrl}/api/classifiers?level=${level}`;
     if (parents) {
       url = url + `&Parents=${parents}`;
     }
