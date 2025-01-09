368c1d8bb0ce7af8ee2e444bba54b61618b995da
Rishab Mehra <rishab.mehra@external.telespazio.com>
2025-01-09 13:54:29 +0000

    feat(frontend): updated the code as per review comments

--- __tests__/config/environmentConfig.spec.ts
+++ __tests__/config/environmentConfig.spec.ts
@@ -50,7 +50,7 @@ describe('Environment environmentConfig', () => {
       } = require('../../src/config/environmentConfig');
       expect(environmentConfig).toBeDefined();
       expect(typeof environmentConfig).toBe('object');
-      expect(Object.keys(environmentConfig).length).toBe(15);
+      expect(Object.keys(environmentConfig).length).toBe(13);
     });
 
     it('should validate and export the configuration object', () => {
