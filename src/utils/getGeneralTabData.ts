d934fc0bf2d349e163601c75cd8c0fb83f393893
Rishab Mehra <rishab.mehra@external.telespazio.com>
2025-02-12 06:38:03 +0000

    feat(frontend): updated the mock data and created a new mapper for UI binding

--- src/utils/getGeneralTabData.ts
+++ src/utils/getGeneralTabData.ts
@@ -5,6 +5,7 @@ import { IGeneralItem } from '../interfaces/searchResponse.interface';
 
 const getResourceLanguages = (data: Record<string, any>): string => {
   const resourceLanguages: string | string[] = data?.resourceLanguage ?? [];
+  //const resourceLanguages: string | string[] = data?.languages ?? [];
   if (
     (typeof resourceLanguages === 'string' && resourceLanguages.length === 0) ||
     (Array.isArray(resourceLanguages) && resourceLanguages.length === 0)
@@ -29,4 +30,4 @@ const getGeneralTabData = (searchItem: Record<string, any>): IGeneralItem => ({
   topicCategories: searchItem?._source?.cl_topic?.map((item) => item.default).join(', ') ?? '',
 });
 
-export { getGeneralTabData };
+export { getGeneralTabData, getResourceLanguages };
