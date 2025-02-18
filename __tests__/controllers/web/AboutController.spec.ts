246dfb6b78df5114cc6eb420897ce7641503f015
Rishab Mehra <rishab.mehra@external.telespazio.com>
2025-02-18 14:19:30 +0000

    feat(frontend): added the test suite for about controller

---
+++ __tests__/controllers/web/AboutController.spec.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+import { Request, ResponseToolkit, ResponseObject } from '@hapi/hapi';
+import { AboutController } from '../../../src/controllers/web/AboutController';
+import { LandingPageData } from '../../../src/utils/constants';
+
+describe('AboutController.AboutController', () => {
+  let request: Partial<Request>;
+  let response: Partial<ResponseToolkit>;
+  beforeEach(() => {
+    request = {};
+
+    response = {
+      view: jest.fn(),
+      response: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render the landing page view', async () => {
+    const result = await AboutController.renderAboutHandler(request as Request, response as ResponseToolkit);
+    expect(response.view).toHaveBeenCalledWith('screens/about/template', LandingPageData);
+  });
+});
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -271,3 +271,32 @@ export const FILTER_VALUES = {
   serviceType: 'svt',
   dataFormat: 'fmt',
 };
+
+export const LandingPageData = {
+  pageHeading: 'NCEA Programme',
+  introText: {
+    content: [
+      'Natural capital is the parts of our natural environment that hold value for people, giving environmental, economic, societal and cultural benefits. It is nature’s stock of living and non-living resources – including ecosystems, species, freshwater, land, soils, minerals, air, and ocean – that provide flows of environmental (ecosystem) services.',
+      'Natural capital is the parts of our natural environment that hold value for people, giving environmental, economic, societal and cultural benefits. It is nature’s stock of living and non-living resources – including ecosystems, species, freshwater, land, soils, minerals, air, and ocean – that provide flows of environmental (ecosystem) services.',
+    ],
+    imageUrl: `${BASE_PATH}/assets/images/introText.jpg`,
+  },
+  categories: [
+    {
+      text: 'NRW’s 10 week public consultation on a new National Park',
+      imgUrl: `${BASE_PATH}/assets/images/newsfp.jpg`,
+    },
+    {
+      text: 'Newborough National Nature Reserve and Forest, Anglesey',
+      imgUrl: `${BASE_PATH}/assets/images/llanddwyn.jpg`,
+    },
+    {
+      text: 'Bwlch Nant yr Arian Visitor Centre, near Aberystwyth',
+      imgUrl: `${BASE_PATH}/assets/images/bnya2.jpg`,
+    },
+    {
+      text: 'Coed y Brenin Visitor Centre, near Dolgellau',
+      imgUrl: `${BASE_PATH}/assets/images/dronecoe.jpg`,
+    },
+  ],
+};
