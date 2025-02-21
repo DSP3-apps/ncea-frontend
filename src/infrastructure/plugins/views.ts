b1939f08dec51678f704901594c028f0c4fdbd91
Rishab Mehra <rishab.mehra@external.telespazio.com>
2025-02-21 17:33:10 +0000

    feat(frontend): added the partner links and logs quick links feeds section

--- src/infrastructure/plugins/views.ts
+++ src/infrastructure/plugins/views.ts
@@ -71,6 +71,7 @@ const customHapiViews = {
     relativeTo: __dirname,
     isCached: process.env.NODE_ENV !== 'production',
     context: {
+      basePath: `${BASE_PATH}`,
       assetPath: `${BASE_PATH}/assets`,
       serviceName: 'Find natural capital data',
       pageTitle: 'Find natural capital data - GOV.UK',
