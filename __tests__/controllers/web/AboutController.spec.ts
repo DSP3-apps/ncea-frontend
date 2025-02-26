8f78d6dc501dc305cc0b5d3c724b8fa9a077e052
Rishab Mehra <rishab.mehra@external.telespazio.com>
2025-02-26 07:45:18 +0000

    feat(frontend): added the feed API failure scenerio

--- __tests__/controllers/web/AboutController.spec.ts
+++ __tests__/controllers/web/AboutController.spec.ts
@@ -24,17 +24,38 @@ describe('AboutController.AboutController', () => {
     jest.resetAllMocks();
   });
 
-  it('should render the landing page view', async () => {
+  it('should render the landing page view if feeds API returns a successfull response', async () => {
     const feedData = { title: 'Feed 1', articles: [] };
-
     (getFeedsData as jest.Mock).mockResolvedValueOnce(feedData);
+
     const dummyResponse: ResponseObject = {} as ResponseObject;
     (response.view as jest.Mock).mockReturnValue(dummyResponse);
+
     const result = await AboutController.renderAboutHandler(request as Request, response as ResponseToolkit);
     expect(response.view).toHaveBeenCalledWith('screens/about/template', {
+      displayFeedsPanel: true,
       feedsList: {
         title: feedData.title,
         articles: [feedData.articles[0]],
+      },
+      ...landingPageData,
+    });
+    expect(result).toBe(dummyResponse);
+  });
+
+  it('should render the landing page view if feeds API returns a failure response', async () => {
+    (getFeedsData as jest.Mock).mockRejectedValue(new Error('Test error'));
+
+    const codeMock = jest.fn();
+    const errorResponse = { code: codeMock };
+    (response.response as jest.Mock).mockReturnValue(errorResponse);
+
+    await AboutController.renderAboutHandler(request as Request, response as ResponseToolkit);
+    expect(response.view).toHaveBeenCalledWith('screens/about/template', {
+      displayFeedsPanel: false,
+      error: {
+        errorTitle: 'An error is occurred while getting the Feeds',
+        errorMessage: 'Something is happened, Please try after sometime',
       },
       ...landingPageData,
     });
--- src/controllers/web/AboutController.ts
+++ src/controllers/web/AboutController.ts
@@ -8,15 +8,27 @@ import { landingPageData, nceaFeedsUrl } from '../../utils/constants';
 
 const AboutController = {
   renderAboutHandler: async (request: Request, response: ResponseToolkit): Promise<ResponseObject> => {
-    const feedsData = await getFeedsData(nceaFeedsUrl);
-
-    return response.view('screens/about/template', {
-      feedsList: {
-        title: feedsData.title,
-        articles: [feedsData.articles[0]],
-      },
-      ...landingPageData,
-    });
+    try {
+      const feedsData = await getFeedsData(nceaFeedsUrl);
+      return response.view('screens/about/template', {
+        displayFeedsPanel: true,
+        feedsList: {
+          title: feedsData.title,
+          articles: [feedsData.articles[0]],
+        },
+        ...landingPageData,
+      });
+    } catch (err) {
+      const errorObj = {
+        errorTitle: 'An error is occurred while getting the Feeds',
+        errorMessage: 'Something is happened, Please try after sometime',
+      };
+      return response.view('screens/about/template', {
+        displayFeedsPanel: false,
+        error: errorObj,
+        ...landingPageData,
+      });
+    }
   },
 };
 
