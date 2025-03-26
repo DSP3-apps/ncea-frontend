8f77f5ed72958192702248728b07fc5b47846f4c
Rishab Mehra <rishab.mehra@external.telespazio.com>
2025-03-26 15:05:09 +0000

    fix(frontend): fix the test suite

--- __tests__/utils/formatSearchResponse.spec.ts
+++ __tests__/utils/formatSearchResponse.spec.ts
@@ -1,5 +1,6 @@
 import { formatSearchResponse } from '../../src/utils/formatSearchResponse';
 import { MORE_INFO_MOCK_DATA } from '../../src/services/handlers/mocks/more-info-response';
+import { naturalTabStaticData } from '../../src/utils/constants';
 
 describe('Format the search response', () => {
   it('should format the search response correctly', async () => {
@@ -94,17 +95,9 @@ describe('Format the search response', () => {
       available_formats: '',
       frequency_of_update: 'unknown',
       character_encoding: 'utf8',
-      Natural_capital_title: 'Exotic Notifible Disease Report Case Reason for Submission Vocabulary',
-      Natural_capital_description:
-        "This dataset provides a codelist for the reason why a sample resulting in Non Negative Lab Result was submitted  that resulted an Exotic Notifiable Disease investigations carried out by the Animal and Plant Health Agency. Investigations that follow the report of an exotic notifiable disease are called 'report cases'. Notifiable diseases in this context are animal diseases that an animal owner/keeper is under legal obligation to report to the Animal and Plant Health Agency (APHA), even if there is only a suspicion that an animal may be affected. 'Exotic diseases' are defined as diseases that are not currently present within the United Kingdom. Reason for Submission is the reason why a sample resulting in Non Negative Lab Result was submitted this could be a result of: \n" +
-        'Active Surveillance These are investigations where a surveillance program is in place that actively samples certain premises in order to provide assurance of national disease freedom.  \n' +
-        'Testing to Exclude: Testing for exclusion is specifically intended for use in cases where NAD or West Nile Fever is not formally suspected, but cannot be excluded from the differential diagnosis of a health or production problem. This service may help to detect disease at the earliest opportunity in those cases where the clinical signs do not give rise to a level of suspicion of a NAD or West Nile Fever that justifies either a consultation case or a statutory notification and official inquiry in the first instance. For more information on testing for exclusion scheme for NAD please see http://ahvla.defra.gov.uk/documents/nad/vr-534.3.full_.pdf  \n' +
-        'Pre Export: Samples submitted as part of import controls\n' +
-        'Post Import: Samples submitted as part of import controls\n' +
-        'Private Samples: Samples collected for private diagnostic work\n' +
-        'Pre Breeding: Samples submitted before an animal breeds\n' +
-        'Not Disclosed: Samples which are submitted for undeclared reason.',
-      Natural_capital_displayData: '',
+      Natural_capital_title: naturalTabStaticData.title,
+      Natural_capital_description: naturalTabStaticData.description,
+      Natural_capital_displayData: expect.any(String),
       Natural_capital_no_data: '',
       Natural_capital_glossary_link: '',
       spatialDataService: '',
